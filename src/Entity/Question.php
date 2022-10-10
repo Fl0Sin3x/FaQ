833757143e4788a4ddb1dc000091e61fda392fd8
Sinex <59825098+Floriansalducci@users.noreply.github.com>
2022-10-11 00:49:22 +0200

    add Tags && Fixture Question

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -34,9 +34,13 @@ class Question
     #[ORM\OneToMany(mappedBy: 'question', targetEntity: Answer::class)]
     private Collection $answers;
 
+    #[ORM\ManyToMany(targetEntity: Tag::class, mappedBy: 'questions')]
+    private Collection $tags;
+
     public function __construct()
     {
         $this->answers = new ArrayCollection();
+        $this->tags = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -130,6 +134,33 @@ class Question
             if ($answer->getQuestion() === $this) {
                 $answer->setQuestion(null);
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): self
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+            $tag->addQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): self
+    {
+        if ($this->tags->removeElement($tag)) {
+            $tag->removeQuestion($this);
         }
 
         return $this;
